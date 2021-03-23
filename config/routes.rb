f748c6fd59afc55d24fa5a512fd31cd3763a1273
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-03-23 11:03:13 -0300

    fix: add specific layout for site

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
@@ -7,7 +10,6 @@ Rails.application.routes.draw do
   end
   devise_for :profiles
   devise_for :admins
-  get 'welcome/index'
 
-  root to: 'welcome#index'
+  root to: 'site/welcome#index'
 end
