e9aba65ac06a4f987ab026629d0f193349c75180
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-03-25 11:02:42 -0300

    feat: create methods to edit admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :admins_backoffice do
-    resources :admins, only: [:index]
     get 'welcome/index' # Dashboard
+    resources :admins, only: [:index, :edit, :update]
   end
   namespace :site do
     get 'welcome/index'
