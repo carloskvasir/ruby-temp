e685892fcf154187a32eb513dce900b99ec37636
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-03-30 18:05:04 -0300

    feat: using cocoon gem

--- app/javascript/packs/admins_backoffice.js
+++ app/javascript/packs/admins_backoffice.js
@@ -4,4 +4,4 @@ global.bootstrap = require('bootstrap/dist/js/bootstrap')
 require('jquery.easing/jquery.easing');
 fontawesome = require('@fortawesome/fontawesome-free/js/all');
 require('bootstrap-growl-ifightcrime/jquery.bootstrap-growl.min');
-require('cocoon')
+require("@nathanvda/cocoon")
