288e7788e131a9c0c7d43ad573a5f573693ae3c5
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-03-31 17:21:30 -0300

    feat: create search function using sql.like

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,5 +1,9 @@
 class Site::SearchController < SiteController
+  include ActiveRecord::Sanitization::ClassMethods
+
   def questions
-    @questions = Question.includes(:answers).page(params[:page])
+    @questions = Question.includes(:answers)
+                         .where('lower(description) LIKE ?', "%#{sanitize_sql_like(params[:term]).downcase}%")
+                         .page(params[:page])
   end
 end
