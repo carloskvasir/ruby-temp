6f224e0891c6bae832d04e4e62ecd252d7ef874b
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-04-01 09:07:46 -0300

    refactor: using scopes in model search

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,17 +7,16 @@ class Question < ApplicationRecord
   paginates_per 5
 
   # Search _term_ in title of all questions with pagination
-  def self.search_in_title(term, page = 0)
-    Question.includes(:answers)
+  scope :search_in_title, -> (term, page = 0) {
+    includes(:answers)
            .where('lower(description) LIKE ?', "%#{sanitize_sql_like(term).downcase}%")
            .page(page)
-
-  end
+  }
 
   # Get latest created questions with pagination
-  def self.latest_questions_in_page(page)
-    Question.includes(:answers)
+  scope :latest_questions_in_page, -> (page) {
+    includes(:answers)
             .order('created_at desc')
             .page(page)
-  end
+  }
 end
