e3ffc46f57c02015c443a9b0ed244f7381899c77
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-04-05 01:48:58 -0300

    feat: using ActionMailer and testing

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -33,7 +33,8 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
 
   def update
     if @admin.update(params_admin)
-          redirect_to admins_backoffice_admins_path, notice: 'Administrador atualizado com sucesso!'
+      AdminMailer.update_email(current_admin, @admin).deliver_now
+      redirect_to admins_backoffice_admins_path, notice: 'Administrador atualizado com sucesso!'
     else
       render :edit
     end
