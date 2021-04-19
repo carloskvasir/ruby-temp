a25e84b0290a35c3922ce4b15fa2d5c5dd2b2ac7
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-04-18 23:18:06 -0300

    fix: error in new autoloader

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,7 @@ module TimeToAnswer
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files
+    config.autoloader = :classic
     config.generators.system_tests = nil
   end
 end
